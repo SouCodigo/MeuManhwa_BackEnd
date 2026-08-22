83dade1c4dd2406922a5c7a1cd60645edb3fcc85
SouBeatrizKaroline <111443798+SouBeatrizKaroline@users.noreply.github.com>
2026-08-22 20:48:29 -0300

    refactor: update bancoDeDados.js

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-async function conectaBancoDeDados() {
-    try {
-        console.log('Conexão com o banco de dados iniciou')
-
-        await mongoose.connect(process.env.MONGO_URL)
-
-        console.log('Conexão com o banco de dados feita com sucesso!')
-    } catch(erro) {
-        console.log(erro)
-    }
+async function connectDatabase() {
+  const mongoUrl = process.env.MONGO_URL
+  if (!mongoUrl) throw new Error('A variável MONGO_URL não foi definida.')
+  mongoose.set('strictQuery', true)
+  await mongoose.connect(mongoUrl)
+  console.log('Banco de dados conectado.')
 }
 
-module.exports = conectaBancoDeDados
+module.exports = connectDatabase
